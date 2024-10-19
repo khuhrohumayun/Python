9805ba0726fe24094b1b667301870f9db1dfe3f3
Muhammad Humayun <humayunmuhammad902@gmail.com>
2024-10-19 17:47:20 +0500

    W2 Problems

--- Week_2/Day_2.py
+++ Week_2/Day_2.py
@@ -1,27 +1,61 @@
-import math
+
+    # Count Vowels in a String 
+
+"""
+# Create function to count vowels in a String
+def count_vowels(string):
+    vowel = 'aeiouAEIOU'
+    count = sum(1 for char in string if char in vowel)
+    return count
 
 
-# Mean of a List of Numbers
 
-def calculate_mean(numbers):
-    return sum(numbers) / len(numbers)
+# Example usage: 
+string = input("Enter a string: ")
+print(f"Number of vowels: {count_vowels(string)}")
+"""
+
+
+    # Find the largrest Number in a list 
+
+"""
+# Create funciton for find largest
+def find_largest(numbers):
+    return max(numbers)
+
+
+# Example Usage:
+numbers = [int(x) for x in input("Enter numbers separated by spaces: ").split()]
+print(f"The largest number is: {find_largest(numbers)}")
+"""
 
 
 
-# Standard Deviation of a List of Numbers
+    # Check if a String is a Palindrome
 
-def calculate_std_dev(numbers):
-    mean = sum(numbers) / len(numbers)
-    variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
-    return math.sqrt(variance)
+"""
+# create function to check palindrome
+def is_palindrome(string):
+    return string == string[::-1]
 
 
 # Example usage:
-numbers = [float(x) for x in input("Enter numbers separated by spaces: ")]
-print(f"The standard deviation of the list is: {calculate_std_dev(numbers)}")
+string = input("Enter a string: ")
+if is_palindrome(string):
+    print(f"{string} is a palindrome")
+else:
+    print(f"{string} is not a palindrome")
+"""
 
 
+def is_palindrome(value):
+    value_str = str(value)
+    return value_str == value_str[::-1]
 
-# Example usage:
-numbers = [float(x) for x in input("Enter numbers separated by spaces: ")]
-print(f"The mean of the list is: {calculate_mean(numbers)}")
+
+value = int(input("Enter number: "))
+
+if is_palindrome(value):
+    print(f"{value} is a palindrome")
+else:
+    print(f"{value} is not palindrome")
